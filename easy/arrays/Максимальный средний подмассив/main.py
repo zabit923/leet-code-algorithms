9dce0d94fe50bbfa56ee74e46e367f01c0dee4ef
zabit <xaclafun1991@gmail.com>
2025-03-28 00:52:54 +0300

    .

--- easy/arrays/Максимальный средний подмассив/main.py
+++ easy/arrays/Максимальный средний подмассив/main.py
@@ -3,7 +3,18 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        n = len(nums)
+        cur_sum = sum(nums[:k])
 
+        max_avg = cur_sum / k
+
+        for i in range(k, n):
+            cur_sum += nums[i]
+            cur_sum -= nums[i - k]
+            avg = cur_sum / k
+            max_avg = max(max_avg, avg)
+
+        return max_avg
 
 
 sol = Solution()
