2f232f86abf83312bcebd953aac6a09457f98923
zabit <xaclafun1991@gmail.com>
2025-03-30 22:33:21 +0300

    .

--- Medium/arrays/FindAllDuplicates.py
+++ Medium/arrays/FindAllDuplicates.py
@@ -21,7 +21,13 @@ class Solution:
         return duplicates
 
     def findDuplicates(self, nums: List[int]) -> List[int]:
-
+        res = []
+        for n in nums:
+            n = abs(n)
+            if nums[n-1] < 0:
+                res.append(n)
+            nums[n - 1] = -nums[n-1]
+        return res
 
 
 sol = Solution()
