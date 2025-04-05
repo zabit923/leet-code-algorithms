8b22a47306ffc78b13ab6fc1d791143959119afe
zabit <xaclafun1991@gmail.com>
2025-04-05 15:52:22 +0300

    .

---
+++ Medium/arrays/RotateImage.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+"""
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+"""
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+
+
+
+sol = Solution()
+print(sol.rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
--- Medium/arrays/SpiralMatrix.py
+++ Medium/arrays/SpiralMatrix.py
@@ -13,7 +13,25 @@ Output: [1,2,3,6,9,8,7,4,5]
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
+        rows, cols = len(matrix), len(matrix[0])
+        x, y, dx, dy = 0, 0, 1, 0
+        res = []
+
+        for _ in range(rows * cols):
+            res.append(matrix[y][x])
+            matrix[y][x] = "."
+
+            if (
+                not 0 <= x + dx < cols
+                or not 0 <= y + dy < rows
+                or matrix[y + dy][x + dx] == "."
+            ):
+                dx, dy = -dy, dx
+
+            x += dx
+            y += dy
+
+        return res
 
 
 sol = Solution()
