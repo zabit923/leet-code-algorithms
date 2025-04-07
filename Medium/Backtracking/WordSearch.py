64c06597ffc06bb7e7416c7a149e6442927857dd
zabit <xaclafun1991@gmail.com>
2025-04-07 20:55:47 +0300

    .

--- Medium/Backtracking/WordSearch.py
+++ Medium/Backtracking/WordSearch.py
@@ -3,7 +3,34 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        path = set()
 
+        def dfs(r: int, c: int, i: int) -> bool:
+            if i == len(word):
+                return True
+            if (
+                r < 0 or c < 0 or
+                r >= rows or c >= cols or
+                word[i] != board[r][c] or
+                (r, c) in path
+            ):
+                return False
+            path.add((r, c))
+            res = (
+                dfs(r + 1, c, i + 1) or
+                dfs(r - 1, c, i + 1) or
+                dfs(r, c + 1, i + 1) or
+                dfs(r, c - 1, i + 1)
+            )
+            path.remove((r, c))
+            return res
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
 
 
 sol = Solution()
