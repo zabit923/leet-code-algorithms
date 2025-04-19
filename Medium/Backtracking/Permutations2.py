1a1ff51574530c0103ee0fa1790932309c1aa567
zabit <xaclafun1991@gmail.com>
2025-04-19 22:12:33 +0300

    .

--- Medium/Backtracking/Permutations2.py
+++ Medium/Backtracking/Permutations2.py
@@ -3,7 +3,24 @@ from typing import List
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start):
+            if start == len(nums):
+                res.append(nums[:])
+                return
 
+            unique_set = set()
+
+            for i in range(start, len(nums)):
+                if nums[i] in unique_set:
+                    continue
+                unique_set.add(nums[i])
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        res = []
+        backtrack(0)
+        return res
 
 
 sol = Solution()
